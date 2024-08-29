776573255b74ca344280b34d6215bbdb3aa2c3c0
Mehmet <mehmetext68@gmail.com>
2024-08-29 15:01:26 +0300

    dropdown-menu added

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -2,12 +2,26 @@ import { ChevronDownIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function HeaderMenu() {
   return (
     <div className="gap-6 hidden md:flex">
-      <button className="flex items-center gap-1">
-        Shop <ChevronDownIcon className="w-4 h-4" />
-      </button>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="flex items-center gap-1">
+          Shop <ChevronDownIcon className="w-4 h-4" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>On Sale</DropdownMenuItem>
+          <DropdownMenuItem>New Arrivals</DropdownMenuItem>
+          <DropdownMenuItem>Brands</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
       <Link href="/">On Sale</Link>
       <Link href="/">New Arrivals</Link>
       <Link href="/">Brands</Link>
